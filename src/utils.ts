34b385db05739d6d7b05a75aa344f094b004ab8d
Jake Carrington <jake.carrington@ao.com>
2019-12-20 15:57:05 +0000

    Changed the format function to put all classes on one line

--- src/utils.ts
+++ src/utils.ts
@@ -11,11 +11,21 @@ const createElement = (
     children
   );
 
-const format = (className: TemplateStringsArray, values: any[], props: any) =>
-  values.reduce((prev, curr, i) => {
-    const value = typeof curr === "function" ? curr(props) : curr;
-    return prev + value + className[i + 1];
-  }, className[0]);
+const format = (className: TemplateStringsArray, values: any[], props: any) => {
+  const classes = [];
+  className.forEach(c =>
+    classes.push(
+      ...c
+        .split("\n")
+        .map(c => c.trim())
+        .filter(c => c.length > 0)
+    )
+  );
+  const conditionals = values.map(value =>
+    typeof value === "function" ? value(props) : value
+  );
+  return [...classes, ...conditionals].join(" ");
+};
 
 export const element = (type: string) => (
   className: TemplateStringsArray,
