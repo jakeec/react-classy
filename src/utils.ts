5ed633d9899846f836c1b465853827c19ecfffe1
Jake Carrington <jake.carrington@ao.com>
2019-12-20 13:25:51 +0000

    Added ability to apply classNames conditionally

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,23 @@
 import React from "react";
 
-const createElement = (type: string, className: TemplateStringsArray) => ({
-  children,
-  ...restProps
-}) =>
+const createElement = (
+  type: string,
+  className: TemplateStringsArray,
+  values: any[]
+) => ({ children, ...restProps }) =>
   React.createElement(
     type,
-    { className: format(className), ...restProps },
+    { className: format(className, values, restProps), ...restProps },
     children
   );
 
-const format = (className: TemplateStringsArray) => className.join(" ").trim();
+const format = (className: TemplateStringsArray, values: any[], props: any) =>
+  values.reduce((prev, curr, i) => {
+    const value = typeof curr === "function" ? curr(props) : curr;
+    return prev + value + className[i + 1];
+  }, className[0]);
 
-export const element = (type: string) => (className: TemplateStringsArray) =>
-  createElement(type, className);
+export const element = (type: string) => (
+  className: TemplateStringsArray,
+  ...values: any[]
+) => createElement(type, className, values);
