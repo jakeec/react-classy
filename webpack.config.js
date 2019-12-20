f5646102e4233a20d662b759c3f732eb0ead937d
Jake Carrington <jake.carrington@ditto.ai>
2019-12-20 21:49:55 +0000

    Using webpack for building, changed licence to MIT

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js',
+    library: 'classy',
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+  }
+};
