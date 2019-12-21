6546c13f3009fab21f5453ffa36a208fddb718fb
Jake <jake.ethan.carrington@gmail.com>
2019-12-21 15:29:00 +0000

    Clone (#1)
    * Rewrote to allow classy to be called as a function or call static methods with classy.<tag>
    
    * Moved types out to separate file
    
    * Deleted test folder
    
    * Upped version number

--- build/index.js
+++ build/index.js
@@ -1,21 +1,51 @@
-const fs = require("fs");
-const path = require("path");
-const tags = require("./tags.json");
+const fs = require('fs');
+const path = require('path');
+const tags = require('./tags.json');
 
-const top = `import { element } from "./utils";
-
-export default class classy {
-  `;
-
-const bottom = `
-}`;
-
+const interfaceTags = [];
 const tagMethods = [];
 
 tags.forEach(tag => {
-  tagMethods.push(`static ${tag} = element('${tag}');`);
+  // tagMethods.push(`static ${tag} = element('${tag}');`);
+  interfaceTags.push(`${tag}: any;`);
+  tagMethods.push(`classy['${tag}'] = element('${tag}');`);
 });
 
-const final = `${top}${tagMethods.join("\n  ")}${bottom}`;
+const buildClassy = () => {
+  const top = `import { element, clone } from "./utils";
+import Classy from './types';
 
-fs.writeFileSync(path.resolve(__dirname, "../src/classy.ts"), final);
+const classy = clone;
+`;
+
+  const bottom = `
+  
+export default classy as Classy;`;
+
+  const final = `${top}${tagMethods.join('\n')}${bottom}`;
+
+  fs.writeFileSync(path.resolve(__dirname, '../src/classy.ts'), final);
+};
+
+const buildTypes = () => {
+  const types = `import React from 'react';
+
+type Clone = (
+  elementToWrap: any
+) => (
+  className: any,
+  ...values: any[]
+) => (props: any) => React.FunctionComponentElement<{}>;
+
+interface Static {
+  ${interfaceTags.join('\n  ')}
+}
+
+type Classy = Clone & Static;
+export default Classy;`;
+
+  fs.writeFileSync(path.resolve(__dirname, '../src/types.ts'), types);
+};
+
+buildTypes();
+buildClassy();
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const createElement = (
   type: string,
@@ -11,23 +11,43 @@ const createElement = (
     children
   );
 
+export const copyComponent = (
+  element: React.FunctionComponent,
+  className: TemplateStringsArray,
+  values: any[]
+) => props => {
+  const { children, ...restProps } = props;
+  const el = element(children);
+  values.unshift(el.props.className);
+  return React.createElement(
+    element,
+    { className: format(className, values, restProps), ...restProps },
+    props.children ? props.children : null
+  );
+};
+
 const format = (className: TemplateStringsArray, values: any[], props: any) => {
   const classes = [];
   className.forEach(c =>
     classes.push(
       ...c
-        .split("\n")
+        .split('\n')
         .map(c => c.trim())
         .filter(c => c.length > 0)
     )
   );
   const conditionals = values.map(value =>
-    typeof value === "function" ? value(props) : value
+    typeof value === 'function' ? value(props) : value
   );
-  return [...classes, ...conditionals].join(" ");
+  return [...classes, ...conditionals].join(' ');
 };
 
 export const element = (type: string) => (
   className: TemplateStringsArray,
   ...values: any[]
 ) => createElement(type, className, values);
+
+const clone = elementToWrap => (className, ...values: any[]) =>
+  copyComponent(elementToWrap, className, values);
+
+export { clone };
